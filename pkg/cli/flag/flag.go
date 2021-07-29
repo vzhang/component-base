cb1f84658673b7bab863dd191b62fb6c9cf22b37
Aaron Zhang <mzhang8741@gmail.com>
2021-07-29 23:05:52 +0800

    add flags

---
+++ pkg/cli/flag/flag.go
@@ -0,0 +1,21 @@
+package flag
+
+import (
+	goflag "flag"
+	"github.com/spf13/pflag"
+	"strings"
+)
+
+// WordSepNormalizeFunc changes all flags that contain "_" separators.
+func WordSepNormalizeFunc(f *pflag.FlagSet, name string) pflag.NormalizedName {
+	if strings.Contains(name, "_") {
+		return pflag.NormalizedName(strings.ReplaceAll(name, "_", "-"))
+	}
+	return pflag.NormalizedName(name)
+}
+
+// InitFlags normalizes, parses, then logs the command line flags.
+func InitFlags(flags *pflag.FlagSet) {
+	flags.SetNormalizeFunc(WordSepNormalizeFunc)
+	flags.AddGoFlagSet(goflag.CommandLine)
+}
